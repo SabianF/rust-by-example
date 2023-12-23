fc1b4f6b9b24ef20aeb230bcb87c61674d26b3d5
SabianF <58588133+SabianF@users.noreply.github.com>
2023-12-23 11:54:51 -0800

    feat: display list

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// TODO: Current step 1.2.1 https://doc.rust-lang.org/rust-by-example/hello/print/print_debug.html
+// TODO: Current step 1.2.2 https://doc.rust-lang.org/rust-by-example/hello/print/print_display.html
 
 mod core;
 
@@ -6,10 +6,12 @@ use crate::core::domain::repositories::hello_world::{
   run_formatted_print,
   run_formatted_print_debug,
   run_formatted_print_display,
+  run_formatted_print_display_list,
 };
 
 fn main() {
   run_formatted_print();
   run_formatted_print_debug();
   run_formatted_print_display();
+  run_formatted_print_display_list();
 }
