2f2a18290ab2b970b1105591b17fad3df6c821a5
SabianF <58588133+SabianF@users.noreply.github.com>
2023-12-26 21:08:25 -0800

    feat: formatted print formatting

--- src/core/domain/use_cases/run_1_hello_world_set.rs
+++ src/core/domain/use_cases/run_1_hello_world_set.rs
@@ -1,9 +1,12 @@
 
+// TODO: Current step 1.2.3 https://doc.rust-lang.org/rust-by-example/hello/print/fmt.html 
+
 use crate::core::domain::repositories::hello_world::{
   run_1_2_formatted_print,
   run_1_2_1_formatted_print_debug,
   run_1_2_2_formatted_print_display,
   run_1_2_2_1_formatted_print_display_list,
+  run_1_2_3_formatted_print_formatting,
 };
 
 pub fn main() {
@@ -12,5 +15,6 @@ pub fn main() {
   run_1_2_1_formatted_print_debug();
   run_1_2_2_formatted_print_display();
   run_1_2_2_1_formatted_print_display_list();
+  run_1_2_3_formatted_print_formatting();
   println!("========== 1 - Hello World end ==========");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-// TODO: Current step 1.2.2 https://doc.rust-lang.org/rust-by-example/hello/print/print_display.html
 
 mod core;
 
