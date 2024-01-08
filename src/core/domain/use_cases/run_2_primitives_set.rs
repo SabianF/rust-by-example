bbfcc2f136bfd3f6acb5a54500f8dab424dd362c
SabianF <58588133+SabianF@users.noreply.github.com>
2024-01-07 21:46:53 -0800

    feat: primitives 2.2: Tuples activity 1

--- src/core/domain/use_cases/run_2_primitives_set.rs
+++ src/core/domain/use_cases/run_2_primitives_set.rs
@@ -1,10 +1,12 @@
 
 use crate::core::domain::repositories::primitives::{
   run_2_1_literals_and_operators,
+  run_2_2_tuples,
 };
 
 pub fn main() {
   println!("========== 2 - Primitives start ==========");
   run_2_1_literals_and_operators();
+  run_2_2_tuples();
   println!("========== 2 - Primitives end ==========");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 
+// TODO: Rust by Example 2: https://doc.rust-lang.org/rust-by-example/primitives.html 
+
+// TODO: After completing Rust by Example, the next area I want to explore is graphics programming: https://www.rastertek.com/tutindex.html 
+
 mod core;
 
 use crate::core::domain::use_cases::run_1_hello_world_set;
